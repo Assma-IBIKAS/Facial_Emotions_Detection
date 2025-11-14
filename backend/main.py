0fade7556e094298c740a4a15ef4d0d995db3f3e
Assma-IBIKAS <Assma-IBIKAS>
2025-11-14 12:28:00 +0100

    the second update main

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,18 @@
+
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
-from fastapi import FastAPI, UploadFile, File
-from app.database import Base, engine
+from fastapi import FastAPI, UploadFile, File,Depends
+from app.database import Base, engine, get_db
 from app.models import prediction_model
 from notebooks.HaarCascade import my_model_prediction
 import aiofiles,uuid
 import numpy as np
+from sqlalchemy.orm import Session
+from app.models.prediction_model import Prediction
 
 app = FastAPI()
 
@@ -20,7 +23,8 @@ def Home():
     return { "message" : "Hello from the another side !!"}
 
 @app.post("/predictions")
-async def create_predection(file : UploadFile = File(...)):
+async def create_predection(file : UploadFile = File(...), database : Session = Depends(get_db)):
+
     file_ext = file.filename.split(".")[-1]
     temp_filename = f"{uuid.uuid4()}.{file_ext}"
 
@@ -33,7 +37,35 @@ async def create_predection(file : UploadFile = File(...)):
         confidence = float(confidence)
     elif hasattr(confidence, "numpy"):  # si TensorFlow tensor
         confidence = float(confidence.numpy())
-    return { "confidence" : confidence,
-            "label" : label
+
+    db_pred = Prediction(
+        emotion = label,
+        confidence = confidence
+    )
+
+    database.add(db_pred)
+    database.commit()
+    database.refresh(db_pred)
+
+    return { "confidence" : db_pred.confidence,
+            "label" : db_pred.emotion
             }
+
+@app.get("/History")
+def get_history(database : Session = Depends(get_db)):
+
+    preds = database.query(Prediction).order_by(Prediction.created_at.desc()).all()
+
+    results = []
+    for pred in preds:
+        results.append({
+            "id" : pred.id,
+            "emotion" : pred.emotion,
+            "confidence" : round(pred.confidence,2),
+            "date" : pred.created_at.strftime("%d/%m/%Y %H:%M:%S")
+        })
+
+    return results
+
+
 
